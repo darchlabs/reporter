3b2b42df3380df318f5d8d48eedbee96620a1303
nicobarra <nbarragan01@gmail.com>
2023-02-14 00:02:12 -0300

    feat(reporter.go): updated created at field in camel case

--- reporter.go
+++ reporter.go
@@ -3,20 +3,21 @@ package reporter
 import "time"
 
 type ServiceType string
+
 const (
 	ServiceTypeSynchronizers ServiceType = "synchronizers"
-	ServiceTypeJobs ServiceType = "jobs"
-	ServiceTypeNodes ServiceType = "nodes"
+	ServiceTypeJobs          ServiceType = "jobs"
+	ServiceTypeNodes         ServiceType = "nodes"
 )
 
 type Report struct {
-	ID string `json:"id"`
-	Status string `json:"status"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        string    `json:"id"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 type GroupReport struct {
-	Type ServiceType `json:"type"`
-	Reports []*Report `json:"services"`
-	CreatedAt time.Time `json:"created_at"`
+	Type      ServiceType `json:"type"`
+	Reports   []*Report   `json:"services"`
+	CreatedAt time.Time   `json:"createdAt"`
 }
